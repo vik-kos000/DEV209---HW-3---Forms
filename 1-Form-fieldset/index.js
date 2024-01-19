b22469413f9257c93d1a763bc5ffd00168f2b60d
vik-kos000 <vik.kos000@gmail.com>
2024-01-18 20:40:26 -0800

    most of the code is complete

--- 1-Form-fieldset/index.js
+++ 1-Form-fieldset/index.js
@@ -1,31 +1,59 @@
 
-/* the < fieldset > element provides a grouping for a part of an HTML form, 
-with a nested < legend > element providing a caption for the<fieldset>.
-It takes few attributes, the most notable of which are form, 
-which can contain the id of a < form > on the same page, allowing you to 
-make the < fieldset > part of that < form > even if it is not nested inside it, 
-and disabled, which allows you to disable the < fieldset > and all its contents in one go.
-https://developer.mozilla.org/en-us/docs/web/html/element/fieldset
-*/
-document.addEventListener("DOMContentLoaded", function (event) {
-    // disable the lower part of the form until we have user input for all values
-    document.getElementById("experience").disabled = true;
-})
+document.getElementById("login").addEventListener("click", newUserLogin); 
+document.getElementById("login2").addEventListener("click", oldUserLogin);
+document.getElementById("newUser").addEventListener("click", newUser); 
+document.getElementById("oldUser").addEventListener("click", existingUser);
 
-function formSubmitEvent() {
-    let name = document.getElementById("custname").value;
-    let address = document.getElementById("street").value;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("logIn").disabled = true;
+    document.getElementById("custInfo").disabled = true;
+});
+
+function newUser(event){
+    document.getElementById("custInfo").disabled = false;
+    document.getElementById("logIn").disabled = true;
+    document.getElementById("newOrOldUser").disabled = true;
+}
+
+function existingUser(event){
+    document.getElementById("logIn").disabled = false;
+    document.getElementById("custInfo").disabled = true;
+    document.getElementById("newOrOldUser").disabled = true;
+}
+
+function newUserLogin(event){
+    let UserName = document.getElementById("UserName").value;
+    let Password = document.getElementById("Password").value;
     let city = document.getElementById("city").value;
     let state = document.getElementById("state").value;
-    let zip = document.getElementById("zip").value;
 
-    // we could do much better validation, but that's not the point of this example!
-    if (name.length > 2 && address.length > 2 && city.length > 2 && state.length > 1 && zip.length == 5) {
-        document.getElementById("experience").disabled = false;
-        document.getElementById("cust2").value = name;
-        document.getElementById("visitdate").value = new Date();
+    if (UserName.length > 2 && Password.length > 2 && city.length > 2 && state.length > 1) {
+        alert("Welcome " + UserName);
+
+        localStorage.setItem("UserName", UserName);
+        localStorage.setItem("Password", Password);
+        localStorage.setItem("city", city);
+        localStorage.setItem("state", state);
     }
     else {
         alert("please fill in all fields");
     }
 }
+
+function oldUserLogin(event){
+    let storedUserName = localStorage.getItem("UserName");
+    let storedPassword = localStorage.getItem("Password");
+
+    let logInUserName = document.getElementById("logInUserName").value;
+    let logInPassword = document.getElementById("logInPassword").value;
+
+    if (logInUserName === storedUserName && logInPassword === storedPassword) {
+        alert("Welcome back " + logInUserName);
+    } else {
+        alert("Invalid");
+    }
+}
+
+
+
